4a66e94680cfb8943549bda22018d16cd1cd19de
dr-x <dr-x@mail.com>
2025-08-17 13:32:32 +0800

    fix: change template

--- template-vue/src/router/routes.js
+++ template-vue/src/router/routes.js
@@ -1,9 +1,59 @@
-// 路由
-export default [
-    {path: '/', name: 'Layout', component: () => import('@/layout/index.vue'), children: [
-        {path: '/', name: 'Index', component: () => import('@/view/index.vue'), meta: {title: '主页',},},
-        {path: '/help', name: 'Help', component: () => import('@/view/help/index.vue'), meta: {title: '帮助',},},
-        {path: '/about', name: 'About', component: () => import('@/view/about/index.vue'), meta: {title: '关于',},},
-    ],},
-    {path: '/:path(.*)*', name: 'NotFound', component: () => import('@/view/404.vue'),},
-]
+const routesFormat = (routes) => {
+    routes.sort((a, b) => b.path.split('/').filter(Boolean).length - a.path.split('/').filter(Boolean).length)
+    const nodeMap = new Map(routes.map(s => [s.path, s]))
+    const result = []
+
+    for (const currentNode of routes) {
+        const pathParts = currentNode.path.split('/').filter(Boolean)
+        let parentPath = null
+
+
+        for (let i = pathParts.length - 1; i >= 1; i--) {
+            const parentParts = pathParts.slice(0, i)
+            const candidate = `/${parentParts.join('/')}`
+
+            if (nodeMap.has(candidate)) {
+                parentPath = candidate
+                break
+            }
+        }
+
+        if (parentPath) {
+            const parentNode = nodeMap.get(parentPath)
+            if (parentNode?.children) {
+                parentNode.children.push(currentNode)
+            } else {
+                parentNode.children = [currentNode]
+            }
+        } else {
+            result.push(currentNode)
+        }
+    }
+
+    return result
+}
+
+const clearPath = (list) => {
+    return list.map(i => {
+        i.path = i.path.replaceAll('/index', '') || '/'
+        if (i?.children) i.children = clearPath(i.children)
+        return i
+    })
+}
+
+const getRoutes = () => {
+    const pages = import.meta.glob('../views/**/page.js', {eager: true, import: 'default'})
+    const components = import.meta.glob('../views/**/index.vue')
+    let routes = Object.entries(pages).map(([fullPath, meta]) => {
+        const path = fullPath.replace('../views', '').replace('/page.js', '')
+        const name = path.split('/').filter(Boolean).join('-')
+        const comPath = fullPath.replace('page.js', 'index.vue')
+        return {path, name, component: components[comPath], meta}
+    })
+    routes = routesFormat(routes)
+    routes = clearPath(routes)
+    routes.push({path: '/:path(.*)*', name: 'error', component: () => import('@/views/error/index.vue'),},)
+    return routes
+}
+
+export default getRoutes()
--- template-vue/src/views/index/index.vue
+++ template-vue/src/views/index/index.vue
@@ -1,13 +1,12 @@
 <script setup>
-import {RouterView, useRoute, useRouter} from "vue-router"
+import {RouterView, useRoute} from "vue-router"
 import {computed} from "vue"
 import useSettingStore from "@/store/setting.js";
 
-const router = useRouter()
 const route = useRoute()
 const settingStore = useSettingStore();
 
-const menu = computed(() => router.getRoutes().find(iRoute => iRoute.name === 'Layout').children)
+const menu = computed(() => route.matched.find(s => s.meta?.layout === 'index').children.sort((a, b) => (a.meta?.sort || 0) - (b.meta?.sort || 0)))
 const current = computed(() => route.path)
 </script>
 
@@ -15,12 +14,12 @@ const current = computed(() => route.path)
   <div>
     <el-menu :default-active="current" class="layout-menu" mode="horizontal" router>
       <template v-for="item in menu">
-        <el-menu-item :index="item.path">{{ item?.meta?.title }}</el-menu-item>
+        <el-menu-item :index="item.path">{{ item.meta?.title }}</el-menu-item>
       </template>
     </el-menu>
     <el-scrollbar class="layout-index">
       <RouterView v-slot="{Component, route}">
-        <Transition :name="route?.meta?.transition || settingStore.transition" mode="out-in">
+        <Transition :name="route.meta?.transition || settingStore.transition" mode="out-in">
           <Component :is="Component" :key="route.path"/>
         </Transition>
       </RouterView>
